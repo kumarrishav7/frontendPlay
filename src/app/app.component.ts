1f1f8592269b17044c82fa97bdf562155030a01b
kumarrishav7 <kumarrishav7@gmail.com>
2025-03-14 08:35:21 +0000

    Added spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AnalyseMessageBrokerBehaviourComponent } from './analyse-message-broker-behaviour/analyse-message-broker-behaviour.component';
+import { LoadingService } from './loading.service';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AnalyseMessageBrokerBehaviourComponent],
+  imports: [
+    RouterOutlet,
+    AnalyseMessageBrokerBehaviourComponent,
+    CommonModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  loading$: Observable<boolean>;
   title = 'rishav-playground';
+  constructor(private loadingService: LoadingService) {
+    this.loading$ = this.loadingService.loading$;
+  }
 }
--- src/app/y.interceptor.ts
+++ src/app/y.interceptor.ts
@@ -1,11 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { finalize } from 'rxjs';
+import { LoadingService } from './loading.service';
 
 export const yInterceptor: HttpInterceptorFn = (req, next) => {
+  const loadingService = inject(LoadingService);
+  loadingService.show();
   const modifiedReq = req.clone({
     setHeaders: {
       Authorization: `Bearer YOUR_TOKEN_HERE`,
       'Custom-Header': 'CustomHeaderValue',
     },
   });
-  return next(modifiedReq);
+  return next(modifiedReq).pipe(
+    finalize(() => {
+      loadingService.hide();
+    })
+  );
 };
