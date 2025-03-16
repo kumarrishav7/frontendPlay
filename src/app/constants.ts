9b3c756f6a1133afe957b11a49d51607d8d070f7
kumarrishav7 <kumarrishav7@gmail.com>
2025-03-16 13:34:21 +0000

    Pointing to deployed consumer

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,8 +1,12 @@
 export const BaseUrl = {
   producer:
     'https://rishavplayground-fsdtbkgme6fmagan.canadacentral-01.azurewebsites.net',
-  consumer: 'http://localhost:5224',
+  consumer:
+    'https://consumerplay-dhhdhre4c8hdc3ax.canadacentral-01.azurewebsites.net',
+  deployedConsumer:
+    'https://consumerplay-dhhdhre4c8hdc3ax.canadacentral-01.azurewebsites.net',
   deployedProducer:
     'https://rishavplayground-fsdtbkgme6fmagan.canadacentral-01.azurewebsites.net',
   localProducer: 'http://localhost:5151',
+  localConsumer: 'http://localhost:5224',
 };
